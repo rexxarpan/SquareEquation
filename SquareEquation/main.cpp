8d9ab4cf9fa2cc6acd3580d2dd294d8a28af7c7b
rexxarpan <bezborodov.roma@list.ru>
2020-04-05 19:32:31 +0700

    Это начало

--- SquareEquation/main.cpp
+++ SquareEquation/main.cpp
@@ -2,18 +2,18 @@
 
 int main()
 {
-	float a, b, c;
+	float a{ 3 }, b{ 5 }, c{ 2 };
 
-	std::cin >> a, b, c;
+//	std::cin >> a, b, c;
+	std::cout << "ax2 + bx + c = 0" << std::endl << "a, b, c" << std::endl;
+	cin >> b >> c;
+	// d- дискриминант епта
+	D = b * b - 4 * a * c;
+	std::cout << "ну тут чему равен дискриминант получается" << D << std::endl;
+	std::cout(D<0)
 
-	if (a != 0)
-	{
 
-	}
-	else
-	{
-
-	}
+	 
 
 	return 0;
 }
